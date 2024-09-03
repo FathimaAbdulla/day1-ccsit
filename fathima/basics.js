4c29929a715b2a6e47354d0467a7613cacb709fe
FathimaAbdulla <fathimasadikh888@gmail.com>
2024-09-03 10:47:18 +0530

    Add files via upload

---
+++ fathima/basics.js
@@ -0,0 +1,16 @@
+console.log("temparature")
+temparature=45;
+if(temparature<20)
+    {
+        console.log("cold")
+    }
+
+else if(temparature <= 20 && temparature >=30)
+    {
+        console.log("normal")
+
+    }
+else{
+    console.log("warm")
+
+}
---
+++ fathima/basics1.js
@@ -0,0 +1,6 @@
+let fruit=['orange','mango','apple']
+console.log(fruit)
+console.log(fruit[2])
+fruit.push('kiwi')
+
+console.log(fruit)
